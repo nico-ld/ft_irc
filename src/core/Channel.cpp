46279d49d6acfa3ca3d6b944fe180f356eb2c2f8
afons <amy.fons@hotmail.com>
2026-07-20 19:12:33 +0200

    Fix : Merge conflict

--- src/core/Channel.cpp
+++ src/core/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: afons <afons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 19:50:45 by jdessoli          #+#    #+#             */
-/*   Updated: 2026/07/20 19:09:23 by afons            ###   ########.fr       */
+/*   Updated: 2026/07/20 19:11:55 by afons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,7 @@ Channel::Channel(const Channel& src) {
 }
 
 // Orthodox Canonical Form: Copy Assignment Operator
-<<<<<<< HEAD
-Channel &Channel::operator=(const Channel& src) {
-=======
 Channel& Channel::operator=(const Channel& src) {
->>>>>>> parser
     if (this != &src) {
         _name = src._name;
         _topic = src._topic;
