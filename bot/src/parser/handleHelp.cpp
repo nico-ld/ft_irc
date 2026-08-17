57299638d59a40957acab1aef1dffe724bb57937
nico-ld <nico27ld@gmail.com>
2026-08-17 17:15:42 +0200

    refact(handleHelp): rewrote handleHelp;

--- bot/src/parser/handleHelp.cpp
+++ bot/src/parser/handleHelp.cpp
@@ -6,7 +6,7 @@
 /*   By: nico <nico@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 16:24:03 by nico              #+#    #+#             */
-/*   Updated: 2026/08/12 10:58:25 by nico             ###   ########.fr       */
+/*   Updated: 2026/08/17 17:15:00 by nico             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,16 @@
 
 void handleHelp(t_bot_data &botData) {
 	std::string channel = botData.parser.getParameters()[0];
-	
-	std::string header = "Menu help of RouxBot !\n";
-	std::string cmdAvailable = "Command available : !help, !game, !uno\n";
-	std::string gamePart = "Game command, to create and manage games : \n\t";
-	std::string gameCmd = "- create <name> : To create a new game (e.g. : !game create uno)\n\t- join : join the game in your channel\n\t- start : Start the game when there is enought players\n\t";
-	std::string gameCmd2 = "- leave : Leave the current lobby \n\t- kick <player> : Kick someone from the game";
-	
-	// Concatenate message
-	std::string message;
-	message.append(header + cmdAvailable + gamePart + gameCmd + gameCmd2);
-	
-	sendMessage(botData, channel, message, CLIENT);
 
-	std::string unoPart = "Uno command, to play uno : \n\t";
-	std::string unoCmd = "- rules : Show Uno rules \n\t- hand : Show your hand (by Private message) \n\t- play <card> : Play a card \n\t- challenge : Challenge a wild draw 4 card\n\t- color : Choose the color after a wild";
+	// General informations
+	sendMessage(botData, channel,
+		"RouxBot is an IRC bot that manage Uno and Werewolf (if dev get enought time and motivation). To use it you get 3 command groups : '!game',  '!uno' and '!ww' or '!werewolf'. '!help' is a  independant command. To get special information about Uno or Werewolf each one get 'rules' command. So the correct syntax for rouxbot is '!<command group> <command> [arg]'",
+		CLIENT
+	);
 
-	// Concatenate message
-	message.clear();
-	message.append(unoPart + unoCmd);
-
-	sendMessage(botData, channel, message, CLIENT);
+	// '!game' command description
+	sendMessage(botData, channel,
+		"Here the description of commands in '!game'. 'create <game>', init a new game of Uno or Werewolf; 'join', to join  a game not started yet; 'leave', to leave a game, even if game has started; 'kick <user>', to kick someone from the game (not from the channel); 'start', to start a game when there is enought player; 'end', to end a game in case of problem.",
+		CLIENT
+	);
 }
